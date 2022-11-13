35bd17493b273c4325fa2caddd16205573f12b70
daido1976 <daido1976@gmail.com>
2022-11-13 20:19:13 +0900

    Fix logout process in pentomino

--- pentomino/README.md
+++ pentomino/README.md
@@ -7,3 +7,7 @@ http://www.littleforest.jp/webtext/
 ```sh
 $ php -S localhost:8000 -t pentomino
 ```
+
+## known issue
+
+- 注文完了後にログアウトせず再度カートページに訪問すると、カートに入れた商品が残ったままになっている
--- pentomino/do_logout.php
+++ pentomino/do_logout.php
@@ -1,4 +1,18 @@
 <?php
+session_save_path("./");
 session_start();
+// セッションのグローバル変数の削除
+$_SESSION = array();
+
+// cookie のセッション ID を削除
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// セッションデータ（ファイル）の削除
 session_destroy();
 header('Location: index.html');
